d6d08c8df7c3b0c1a772ad81e3c93c4c671bc560
mameen <mohamed_ameen@hotmail.com>
2023-05-08 22:06:48 -0700

    Saving form state

--- React.JS/job-ai-sistant/src/components/ContentView.js
+++ React.JS/job-ai-sistant/src/components/ContentView.js
@@ -3,10 +3,13 @@ import StartHere from './StartHere';
 import Tab2 from './Tab2';
 import Tab3 from './Tab3';
 
-function ContentView({ selectedTab, setActiveTab }) {
+function ContentView({ selectedTab, setActiveTab, formValues, handleFormChange }) {
   switch (selectedTab) {
     case 0:
-      return <StartHere setActiveTab={setActiveTab} />;
+      return <StartHere 
+        setActiveTab={setActiveTab}
+        formValues={formValues}
+        handleFormChange={handleFormChange} />;
     case 1:
       return <Tab2 />;
     case 2:
